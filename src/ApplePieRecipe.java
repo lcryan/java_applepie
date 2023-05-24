38172d2138dacb803f37f647f5543d476048fcfe
LuciaRyan <lcryan06@gmail.com>
2023-05-24 12:30:25 +0200

    ingredients as instances now - checking on printing messages - getting error

--- src/ApplePieRecipe.java
+++ src/ApplePieRecipe.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ApplePieRecipe {
+import java.util.function.IntBinaryOperator;
+
+class ApplePieRecipe {
+    Ingredient ongezoutenBoter = new Ingredient(200.0, "gram", "ongezouten boter");
+    Ingredient witteSuiker = new Ingredient(200, "gram", "witte basterd suiker");
+    Ingredient zelfrijzendBakmeel = new Ingredient(400, "gram", "zelfrijzend bakmeel");
+    Ingredient ei = new Ingredient(1, "stuk", "ei");
+    Ingredient vanillesuiker = new Ingredient(8, "gram", "vanillesuiker");
+    Ingredient zout = new Ingredient(1, "snuf", "zout");
+    Ingredient zoetZureAppels = new Ingredient(1.5, "kilo", "zoetzure appels");
+    Ingredient kristalSuiker = new Ingredient(75, "gram", "kristal suiker");
+    Ingredient kaneel = new Ingredient(3, "theelepels", "kaneel");
+    Ingredient paneerMeel = new Ingredient(15, "gram", "paneermeel");
+
+
+//200 gram ongezouten roomboter 200 gram witte bastard suiker 400 gram zelfrijzend bakmeel 1 stuk(s) ei 8 gram vanillesuiker 1 snuf zout 1.5 kilo zoetzure appels
+// 75 gram kristal suiker 3 theelepels kaneel 15 gram paneermeel Stappen:
+
+    public ApplePieRecipe() {
+        this.ongezoutenBoter = new Ingredient(200, "gram", "ongezouten roomboter");
+    }
+
+    public void printIngredients() {
+        System.out.println(ongezoutenBoter.getAmount());
+        System.out.println();
+    }
+
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Ingredient {
+class Ingredient {
+    double amount;
+    String unit;
+    String name;
+
+    public Ingredient() {
+
+    }
+    public Ingredient(double amount, String unit, String name) {
+        this.amount = amount;
+        this.name = name;
+        this.unit = unit;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
 }
+
+
