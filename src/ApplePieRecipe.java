c93b779afe5f19ee758222f9e66a169edf4f7723
LuciaRyan <lcryan06@gmail.com>
2023-05-25 11:17:00 +0200

    added uncommented text recipe

--- src/ApplePieRecipe.java
+++ src/ApplePieRecipe.java
@@ -51,12 +51,30 @@ class ApplePieRecipe {
         System.out.println("• Klop het ei los en verdeel deze in twee delen. De ene helft is voor het deeg, het andere deel is voor het bestrijken van de appeltaart");
     }
 
-    public void stepThree
+    public void stepThreeAddFlour() {
+        System.out.println("Meng de boter, bastard suiker, zelfrijzend bakmeel, een helft van het ei, vanille suiker en een snufje zout tot een stevig deeg en verdeel deze in 3 gelijke delen.");
+    }
+
+    public void stepFour() {
+        System.out.println();
+    }
 
     public void printRecipeSteps() {
         stepOneHeatUpOven();
         stepTwoAddEgg();
+        stepThreeAddFlour();
+
 
     }
 
 }
+
+//Schil nu de appels en snij deze in plakjes. Vermeng in een kopje de suiker en kaneel.
+//  Vet de springvorm in en bestrooi deze met bloem
+  //Gebruik een deel van het deeg om de bodem van de vorm te bedekken. Gebruik een deel van het deeg om de rand van de springvorm te bekleden. Strooi het paneermeel op de bodem van de beklede vorm. De paneermeel neemt het vocht van de appels op.
+
+/*Doe de heft van de appels in de vorm en strooi hier 1/3 van het kaneel-suiker mengsel overheen. Meng de ander helft van de appels met het overge*//*bleven kaneel-suiker mengsel en leg deze in de vorm.*/
+/*Rol het laatste deel van de deeg uit tot een dunne lap en snij stroken van ongeveer 1 cm breed.*/
+/*Leg de stroken kuislings op de appeltaart. Met wat extra deegstroken werk je de rand rondom af. Gebruik het overgebleven ei om de bovenkant van het deeg te bestrijken
+/*
+Zet de taart iets onder het midden van de oven. Bak de taart in 60 minuten op 170 graden Celsius (boven en onderwarmte) gaar en goudbruin.*/
